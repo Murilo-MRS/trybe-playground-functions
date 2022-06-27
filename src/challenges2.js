c8ca394bf8326b31cc60b461519e02be44195340
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-27 20:13:42 -0300

    add 12 exercicio techlist

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,10 +4,20 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  let triangle = false;
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
+    triangle = true;
+  } else if (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) {
+    triangle = true;
+  } else if (lineC < lineA + lineB && lineC > Math.abs(lineA - lineB)) {
+    triangle = true;
+  } else {
+    triangle = false;
+  }
+  return triangle;
 }
-
 // Desafio 13
 function hydrate() {
   // seu código aqui
