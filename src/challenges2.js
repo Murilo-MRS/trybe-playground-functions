a47f9143b4fb6e47c876971b557ceb8f84ca327e
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-28 12:43:26 -0300

    add 11 exercicio generatePhoneNumber correcoes eslint :rocket:

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,19 +1,18 @@
 // Desafio 11
 function generatePhoneNumber(array) {
   // seu código aqui
-  if (array.length != 11) {
-    return 'Array com tamanho incorreto.'
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
   }
-
 
   for (let index = 0; index < array.length; index += 1) {
     let contNum = 0;
-    for (let index2 = 0; index2 < array.length; index2++) {
+    for (let index2 = 0; index2 < array.length; index2 += 1) {
       if (array[index] === array[index2]) {
         contNum += 1;
       }
       if (array[index] < 0 || array[index] > 9 || contNum >= 3) {
-        return 'não é possível gerar um número de telefone com esses valores'
+        return 'não é possível gerar um número de telefone com esses valores';
       } 
     }
   } 
@@ -40,16 +39,16 @@ function triangleCheck(lineA, lineB, lineC) {
 function hydrate(string) {
   // seu código aqui
   let frase = string;
-  let num = frase.match(/\d+/g)
+  let num = frase.match(/\d+/g);
   let numAgua = 0;
 
-  for (index = 0; index < num.length; index += 1) {
+  for (let index = 0; index < num.length; index += 1) {
     numAgua += Number(num[index]);
   }
   if (numAgua > 1) {
-    return numAgua + ' copos de água'
+    return numAgua + ' copos de água';
   }
-  return numAgua + ' copo de água'
+  return numAgua + ' copo de água';
 }
 
 module.exports = {
